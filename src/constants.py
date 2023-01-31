c937292edd2b879ff2da1352ae2435b640606f9a
Matt <mattpuchalski5@gmail.com>
2023-01-30 21:02:41 -0500

    pared down config yaml

--- src/constants.py
+++ src/constants.py
@@ -62,7 +62,7 @@ SECONDS_DELAY_RECEIVING_MSG = (
     3  # give a delay for the bot to respond so it can catch multiple messages
 )
 MAX_THREAD_MESSAGES = 200
-ACTIVATE_THREAD_PREFX = "💬✅"
+ACTIVATE_THREAD_PREFX = "🤠/🤖"
 INACTIVATE_THREAD_PREFIX = "💬❌"
 MAX_CHARS_PER_REPLY_MSG = (
     1500  # discord has a 2k limit, we just break message into 1.5k
