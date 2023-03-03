6fab0da921438cd3a8c3b279cd895963c54432c0
Matt <76557713+mattpuchalski@users.noreply.github.com>
2023-03-02 19:54:55 -0500

    Update completion.py
    we're bumping the model from Davinci to TURBO 3.5!!

--- src/completion.py
+++ src/completion.py
@@ -49,7 +49,7 @@ async def generate_completion_response(
         )
         rendered = prompt.render()
         response = openai.Completion.create(
-            engine="text-davinci-003",
+            engine="gpt-3.5-turbo",
             prompt=rendered,
             temperature=1.0,
             top_p=0.9,
